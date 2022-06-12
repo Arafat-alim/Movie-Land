f71e10d59295f0142f734ac62f48ef9b4086af83
Arafat <arafat97alim@gmail.com>
2022-06-12 22:09:20 +0530

    added lifecycle hooks.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
 
 //! converted into Arrow function
 //create a counter using state
 const App = () => {
+  const [counter, setCounter] = useState(0);
+  //using React function lifeCycle
+  //! Mounting State
+  useEffect(() => {});
+
+  //! Updating state
+  useEffect(() => {}, []);
+
   return (
     <div className="App">
-      <button>+</button>
-      <h2>Counter:0 </h2>
-      <button>-</button>
+      <button onClick={() => setCounter((prevCount) => prevCount + 1)}>
+        +
+      </button>
+      <h2>Counter:{counter} </h2>
+      <button onClick={() => setCounter((prevCount) => prevCount - 1)}>
+        -
+      </button>
     </div>
   );
 };
---
+++ src/practice/lifecyclehooks.js
@@ -0,0 +1,8 @@
+import { useEffect } from "react";
+
+//using React function lifeCycle
+//! Mounting State
+useEffect(() => {});
+
+//! Updating state
+useEffect(() => {}, []);
