ba9ef44afa50eaeba7096bb3de2327cc60289ea3
Arafat <arafat97alim@gmail.com>
2022-06-12 21:54:15 +0530

    added basic topics from react.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
-function App() {
+//! converted into Arrow function
+//create a counter using state
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button>+</button>
+      <h2>Counter:0 </h2>
+      <button>-</button>
     </div>
   );
-}
+};
 
 export default App;
