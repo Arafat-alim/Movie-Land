22e26f7c57060845eae21577dd31d46233137b1f
Arafat <arafat97alim@gmail.com>
2022-06-12 23:21:01 +0530

    created two major files of react - index.js and App.js

---
+++ src/index.js
@@ -0,0 +1,6 @@
+//index.js contains the react-dom
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+
+ReactDOM.render(<App />, document.getElementById("root"));
