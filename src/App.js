b127e00100e1e8470876a6b15cc2f9a6808e9148
Arafat <arafat97alim@gmail.com>
2022-06-13 01:19:48 +0530

    fetched API using OMDB Api

--- src/App.js
+++ src/App.js
@@ -1,6 +1,18 @@
 import React from "react";
+import { useEffect } from "react";
 
 const App = () => {
+  const API_URL = "http://www.omdbapi.com/?i=tt3896198&apikey=6bc9702a";
+  let searchMovies = async (title) => {
+    let response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+    console.log(data.Search);
+  };
+
+  //using useEffect for api calling - component did mount
+  useEffect(() => {
+    searchMovies("Spiderman");
+  }, []);
   return (
     <div>
       <h1>Hello React</h1>
