669c05ce87e5fdfff215ed37ebd2216ea2eee524
Arafat <arafat97alim@gmail.com>
2022-06-13 13:43:35 +0530

    search map to each elements.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
 import React from "react";
 import "./App.css";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import searchIcon from "./searchIcon.svg";
 import MovieCard from "./MovieCard";
 
 const App = () => {
+  //creating state
+  const [movies, setMovies] = useState([]);
   const API_URL = "http://www.omdbapi.com/?i=tt3896198&apikey=6bc9702a";
   let searchMovies = async (title) => {
     let response = await fetch(`${API_URL}&s=${title}`);
     const data = await response.json();
-    console.log(data.Search);
+    // console.log(data.Search);
+    setMovies(data.Search);
   };
   //movide Object
-  const movie1 = {
-    Title: "Italian Spiderman",
-    Year: "2007",
-    imdbID: "tt2705436",
-    Type: "movie",
-    Poster:
-      "https://m.media-amazon.com/images/M/MV5BYjFhN2RjZTctMzA2Ni00NzE2LWJmYjMtNDAyYTllOTkyMmY3XkEyXkFqcGdeQXVyNTA0OTU0OTQ@._V1_SX300.jpg",
-  };
+  // const movie1 = {
+  //   Title: "Italian Spiderman",
+  //   Year: "2007",
+  //   imdbID: "tt2705436",
+  //   Type: "movie",
+  //   Poster:
+  //     "https://m.media-amazon.com/images/M/MV5BYjFhN2RjZTctMzA2Ni00NzE2LWJmYjMtNDAyYTllOTkyMmY3XkEyXkFqcGdeQXVyNTA0OTU0OTQ@._V1_SX300.jpg",
+  // };
   //using useEffect for api calling - component did mount
   useEffect(() => {
     searchMovies("Spiderman");
@@ -36,16 +39,18 @@ const App = () => {
         />
         <img src={searchIcon} alt="search" onClick={() => {}} />
       </div>
-      <div className="container">
-        {/* <MovieCard
-          poster={movie1.Poster}
-          title={movie1.Title}
-          type={movie1.Type}
-          imdbID={movie1.imdbID}
-          year={movie1.Year}
-        /> */}
-        <MovieCard movie1={movie1} />
-      </div>
+
+      {movies?.length > 0 ? (
+        <div className="container">
+          {movies.map((movie) => (
+            <MovieCard movie={movie} />
+          ))}
+        </div>
+      ) : (
+        <div className="empty">
+          <h2>No Movies Found</h2>
+        </div>
+      )}
     </div>
   );
 };
