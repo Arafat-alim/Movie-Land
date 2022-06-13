8c9608e1f680d1d486f413c7277f6a19e4bca648
Arafat <arafat97alim@gmail.com>
2022-06-13 13:13:33 +0530

    added new component named MovieCard and import it into app component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React from "react";
+import "./App.css";
 import { useEffect } from "react";
+import searchIcon from "./searchIcon.svg";
+import MovieCard from "./MovieCard";
 
 const App = () => {
   const API_URL = "http://www.omdbapi.com/?i=tt3896198&apikey=6bc9702a";
@@ -8,15 +11,32 @@ const App = () => {
     const data = await response.json();
     console.log(data.Search);
   };
-
+  //movide Object
+  const movie1 = {
+    Title: "Italian Spiderman",
+    Year: "2007",
+    imdbID: "tt2705436",
+    Type: "movie",
+    Poster:
+      "https://m.media-amazon.com/images/M/MV5BYjFhN2RjZTctMzA2Ni00NzE2LWJmYjMtNDAyYTllOTkyMmY3XkEyXkFqcGdeQXVyNTA0OTU0OTQ@._V1_SX300.jpg",
+  };
   //using useEffect for api calling - component did mount
   useEffect(() => {
     searchMovies("Spiderman");
   }, []);
   return (
-    <div>
-      <h1>Hello React</h1>
-      <p>Happy Coding</p>
+    <div className="app">
+      <h1>Movie Land</h1>
+      <div className="search">
+        <input
+          type="text"
+          placeholder="Search for  movies"
+          value="superman"
+          onChange={() => {}}
+        />
+        <img src={searchIcon} alt="search" onClick={() => {}} />
+      </div>
+      <div className="container"></div>
     </div>
   );
 };
